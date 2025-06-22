ee7c4c2d3e787e65fa6e72642ed90aa876e3414d
PrabuNig <prabu@nigsoft.com>
2025-06-22 16:20:26 +0530

    ui changes

--- lib/api-config.ts
+++ lib/api-config.ts
@@ -1,6 +1,6 @@
 const config = {
-  // BASE_URL: "https://stg-api.superjetom.com",
-  BASE_URL: "http://localhost:3001",
+  BASE_URL: "https://stg-api.superjetom.com",
+  // BASE_URL: "http://localhost:3000",
 };
 
 export default config;
