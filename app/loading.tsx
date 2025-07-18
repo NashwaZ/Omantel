8ef1009068b1996aab763f506b64262aaff47eae
Yukenthiran-28 <yukenthiran@nigsoft.com>
2025-07-18 12:02:11 +0530

    updated lang issues

---
+++ app/loading.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import LoadingIndicator from "@/components/loading-indicator"
+import { useTranslation } from "react-i18next"
+import  "@/lib/i18n"
+
+export default function Loading() {
+    const { t,i18n } = useTranslation();
+    return <LoadingIndicator fullScreen text={t("Loading...")} size="large" />
+}
